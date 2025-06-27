a0954fbc6d81831f89818a5229800cc321fb1b25
leewhsien <leewh@510.org.tw>
2025-06-27 18:27:41 +0800

    Update main.py 完整版50

--- main.py
+++ main.py
@@ -377,5 +377,6 @@ async def callback(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
+
